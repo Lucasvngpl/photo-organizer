0fa1f12064b4b8f16cc24f61369063874c850608
Lucasvngpl <lucas.venugopal@gmail.com>
2026-01-17 15:17:40 -0500

    early stage EfficientNet

--- ml-train/CONV-MAIN.py
+++ ml-train/CONV-MAIN.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import warnings
+from tensorflow.keras.applications import EfficientNetB0
 
 class Conv:
     def __init__(self):
@@ -22,21 +23,35 @@ class Conv:
 
         return gpus
 
-    def new_model(self, input_shape=(4032, 4032)):
+    def new_model(self, input_shape=(512, 512)):
+
+        # Load pre-trained EfficientNetB0
+        base_model = tf.keras.applications.EfficientNetB0(
+            include_top=False,      # Remove ImageNet classifier so it replaces the original classifier classes with our own
+            weights="imagenet",     # Use pre-trained weights
+            input_shape=input_shape,
+            pooling="avg"           # Global average pooling → 1280 features
+        )
+        
+        # Freeze base model
+        base_model.trainable = False
+        
+        # Build: EfficientNet → Dense(128) → Dense(1)
         model = tf.keras.models.Sequential([
-            tf.keras.layers.Input(shape=input_shape),
-
-            tf.keras.layers.Conv2D(filters=256, kernel_size=(3, 3), activation="relu", padding="same"),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-            tf.keras.layers.Dropout(0.3),
-
-            tf.keras.layers.GlobalAveragePooling2D(),
-            tf.keras.layers.Dense(64, activation="relu"),
-            #tf.keras.layers.Dense(num_classes, activation="softmax")
+            base_model,
+            tf.keras.layers.Dropout(0.3), 
+            tf.keras.layers.Dense(128, activation="relu"), # Newly added dense layer with 128 units
+            tf.keras.layers.Dense(1, activation="sigmoid") # Final output layer for binary classification
         ])
+    
 
         return model
+    def compile_model(self): # tells keras how to train the model
+        self.model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), # Adam optimizer with learning rate, updates weights based on loss
+            loss="binary_crossentropy", # loss function for binary classification
+            metrics=["accuracy"] # track accuracy during training   
+        )
 
 
 if __name__ == "__main__":
